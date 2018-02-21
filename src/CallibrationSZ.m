b62bb3becfe050c836450f853130177282a62263
Siniva Areta <sareta@zagmail.gonzaga.edu>
2018-02-20 21:32:14 -0800

    Finished calibration function and logged multiple points into the gameboard which I then saved as Gameboard followed by 8 digits of the date.

--- src/CallibrationSZ.m
+++ src/CallibrationSZ.m
@@ -3,6 +3,7 @@
 %load('GameBoard')
 %GameBoard{r,c}= ScorGetXYZPR
 %end
+function Gameboard=CallibrationSZ(r,c)
 Names={'A1' 'A2' 'A3' 'A4' 'A5' 'A6' 'A7' 'A8';...
     'B1' 'B2' 'B3' 'B4' 'B5' 'B6' 'B7' 'B8';...
     'C1' 'C2' 'C3' 'C4' 'C5' 'C6' 'C7' 'C8';...
@@ -11,11 +12,10 @@ Names={'A1' 'A2' 'A3' 'A4' 'A5' 'A6' 'A7' 'A8';...
     'F1' 'F2' 'F3' 'F4' 'F5' 'F6' 'F7' 'F8';...
     'G1' 'G2' 'G3' 'G4' 'G5' 'G6' 'G7' 'G8';...
     'H1' 'H2' 'H3' 'H4' 'H5' 'H6' 'H7' 'H8'};
-function CallibrationSZ(r,c);
 name=genvarname(Names{r,c});
-Gameboard.(name).xyz=ScorGetXYZPR;
-Gameboard.(name).joints=ScorGetBSEPR;
-Gameboard.(name).pose=ScorGetPose;
-Gameboard.(name).gripper=ScorGetGripper;
-Gameboard.(name).speed=ScorGetSpeed;
+Gameboard.xyz=ScorGetXYZPR;
+Gameboard.joints=ScorGetBSEPR;
+Gameboard.pose=ScorGetPose;
+Gameboard.gripper=ScorGetGripper;
+Gameboard.speed=ScorGetSpeed;
 end
