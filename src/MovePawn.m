2ad5269e4617b976e5a0bd0e65c7a8a7b8cfc0db
Siniva Areta <sareta@zagmail.gonzaga.edu>
2018-04-06 19:00:53 -0700

    Created a function to pick up a pawn and move it. Haven't tested yet.

---
+++ src/MovePawn.m
@@ -0,0 +1,22 @@
+%Pawn moving
+%Lets figure out a way to make a seperate cell where each point contains
+%either a 1 or a 0 signifying if that spot on the chessboard is occupied or
+%not.
+
+function confirm = MovePawn(old,new)
+stop1=[Gameboard2.(old).xyz(1) Gameboard2.(old).xyz(2) Gameboard2.(old).xyz(3)+150 ...
+    Gameboard2.(old).xyz(4) Gameboard2.(old).xyz(5)];
+ScorSetXYZPR(stop1);
+x=18.3;
+ScorSetGripper(x+2);
+ScorSetXYZPR(Gameboard2.(old).xyz);
+ScorSetGripper(x);
+ScorSetXYZPR(stop1);
+stop2=[Gameboard2.(new).xyz(1) Gameboard2.(new).xyz(2) Gameboard2.(new).xyz(3)+150 ...
+    Gameboard2.(new).xyz(4) Gameboard2.(new).xyz(5)];
+ScorSetXYZPR(stop2);
+ScorSetXYZPR(Gameboard2.(new).xyz);
+ScorSetGripper(x+2);
+ScorSetXYZPR(stop2);
+ScorGoHome;
+ans=1
