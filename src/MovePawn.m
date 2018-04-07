032206c6b5c5467c382f23c7a4f6586ec6537d68
Siniva Areta <sareta@zagmail.gonzaga.edu>
2018-04-07 12:24:08 -0700

    Made a testing program for move pawn and it worked! Now we just need to figure out how to make the input of a function output the necessary movements.

--- src/MovePawn.m
+++ src/MovePawn.m
@@ -3,7 +3,9 @@
 %either a 1 or a 0 signifying if that spot on the chessboard is occupied or
 %not.
 
-function confirm = MovePawn(old,new)
+function confirm = MovePawn(old1,new1)
+old=genvarname(old1);
+new=genvarname(new1);
 stop1=[Gameboard2.(old).xyz(1) Gameboard2.(old).xyz(2) Gameboard2.(old).xyz(3)+150 ...
     Gameboard2.(old).xyz(4) Gameboard2.(old).xyz(5)];
 ScorSetXYZPR(stop1);
@@ -19,4 +21,4 @@ ScorSetXYZPR(Gameboard2.(new).xyz);
 ScorSetGripper(x+2);
 ScorSetXYZPR(stop2);
 ScorGoHome;
-ans=1
+confirm=true
---
+++ src/MovePawnTest.m
@@ -0,0 +1,28 @@
+stop1=[Gameboard2.(old).xyz(1) Gameboard2.(old).xyz(2) Gameboard2.(old).xyz(3)+150 ...
+    Gameboard2.(old).xyz(4) Gameboard2.(old).xyz(5)];
+ScorSetXYZPR(stop1);
+x=18.3;
+ScorWaitForMove;
+ScorSetGripper(x+2);
+pick1=[Gameboard2.(old).xyz(1) Gameboard2.(old).xyz(2) Gameboard2.(old).xyz(3)+43.7 ...
+    Gameboard2.(old).xyz(4) Gameboard2.(old).xyz(5)]
+ScorWaitForMove;
+ScorSetXYZPR(pick1);
+ScorWaitForMove;
+ScorSetGripper(x);
+ScorWaitForMove;
+ScorSetXYZPR(stop1);
+stop2=[Gameboard2.(new).xyz(1) Gameboard2.(new).xyz(2) Gameboard2.(new).xyz(3)+150 ...
+    Gameboard2.(new).xyz(4) Gameboard2.(new).xyz(5)];
+ScorWaitForMove;
+ScorSetXYZPR(stop2);
+pick2=[Gameboard2.(new).xyz(1) Gameboard2.(new).xyz(2) Gameboard2.(new).xyz(3)+43.7 ...
+    Gameboard2.(new).xyz(4) Gameboard2.(new).xyz(5)]
+ScorWaitForMove;
+ScorSetXYZPR(pick2);
+ScorWaitForMove;
+ScorSetGripper(x+2);
+ScorWaitForMove;
+ScorSetXYZPR(stop2);
+ScorWaitForMove;
+ScorGoHome;
