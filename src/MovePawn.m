b8e60a3595f65ad420dcaa32eaef30c9536aa740
Siniva Areta <sareta@zagmail.gonzaga.edu>
2018-04-07 12:51:16 -0700

    Added load gameboard so it works now!

--- src/MovePawn.m
+++ src/MovePawn.m
@@ -3,22 +3,34 @@
 %either a 1 or a 0 signifying if that spot on the chessboard is occupied or
 %not.
 
-function confirm = MovePawn(old1,new1)
-old=genvarname(old1);
-new=genvarname(new1);
-stop1=[Gameboard2.(old).xyz(1) Gameboard2.(old).xyz(2) Gameboard2.(old).xyz(3)+150 ...
+function confirm = MovePawn(old,new)
+load('Gameboard2')
+stop1=[Gameboard2.(old).xyz(1) Gameboard2.(old).xyz(2) Gameboard2.(old).xyz(3)+160 ...
     Gameboard2.(old).xyz(4) Gameboard2.(old).xyz(5)];
 ScorSetXYZPR(stop1);
 x=18.3;
+ScorWaitForMove;
 ScorSetGripper(x+2);
-ScorSetXYZPR(Gameboard2.(old).xyz);
+pick1=[Gameboard2.(old).xyz(1) Gameboard2.(old).xyz(2) Gameboard2.(old).xyz(3)+43.7 ...
+    Gameboard2.(old).xyz(4) Gameboard2.(old).xyz(5)]
+ScorWaitForMove;
+ScorSetXYZPR(pick1);
+ScorWaitForMove;
 ScorSetGripper(x);
+ScorWaitForMove;
 ScorSetXYZPR(stop1);
-stop2=[Gameboard2.(new).xyz(1) Gameboard2.(new).xyz(2) Gameboard2.(new).xyz(3)+150 ...
+stop2=[Gameboard2.(new).xyz(1) Gameboard2.(new).xyz(2) Gameboard2.(new).xyz(3)+160 ...
     Gameboard2.(new).xyz(4) Gameboard2.(new).xyz(5)];
+ScorWaitForMove;
 ScorSetXYZPR(stop2);
-ScorSetXYZPR(Gameboard2.(new).xyz);
+pick2=[Gameboard2.(new).xyz(1) Gameboard2.(new).xyz(2) Gameboard2.(new).xyz(3)+43.7 ...
+    Gameboard2.(new).xyz(4) Gameboard2.(new).xyz(5)]
+ScorWaitForMove;
+ScorSetXYZPR(pick2);
+ScorWaitForMove;
 ScorSetGripper(x+2);
+ScorWaitForMove;
 ScorSetXYZPR(stop2);
+ScorWaitForMove;
 ScorGoHome;
 confirm=true
