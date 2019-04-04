88e9b5e2854e208b2051aa23e417a4cbc4321cf5
Siniva (Lab) <sareta@zagmail.gonzaga.edu>
2019-04-04 14:28:03 -0700

    Lab Meeting April 4

--- src/InitializeGame.m
+++ src/InitializeGame.m
@@ -1,7 +1,7 @@
 function [Gameboard, Gamesettings, Chessboard]  =  InitializeGame(filename_Gameboard, filename_Gamesettings)
 
 temp = load(filename_Gameboard,'Board_Locations');
-Gameboard = temp.Gameboard2;
+Gameboard = temp.Board_Locations;
 temp = load(filename_Gamesettings, 'Gamesettings');
 Gamesettings = temp.Gamesettings;
 Chessboard = ResetChessboard();
